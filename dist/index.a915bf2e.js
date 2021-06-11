9331977cb112d5cfe1b84ebb872be8792e688b62
India Thomson <itho5539@uni.sydney.edu.au>
2021-06-11 18:00:51 +0930

    cleaning up

--- dist/index.a915bf2e.js
+++ dist/index.a915bf2e.js
@@ -442,41 +442,7 @@ id) /*: string*/
 }
 
 },{}],"4B4Nd":[function(require,module,exports) {
-const TIME_LIMIT = 1200;
-let timePassed = 0;
-let timeLeft = TIME_LIMIT;
-let timerInterval = null;
 
-document.getElementById("clock").innerHTML 
-
-function formatTime(time) {
-    let minutes = Math.floor(time/40)
-    let seconds = time%60
-    if (seconds < 10) {
-        seconds = `0${seconds}`
-    }
-    if (minutes < 10) {
-        minutes = `0${minutes}`
-    }
-    return `${minutes}: ${seconds}`
-}
-function startTimer() {
-        timerInterval = setInterval(()=> {
-            timePassed += 1;
-            timeLeft = TIME_LIMIT - timePassed;
-            document.getElementById("label").innerHTML =
-                formatTime(timeLeft);
-            if (timeLeft == 0) {
-                stopTimer()
-            }
-            
-        },1000);
- 
-}
-function stopTimer() {
-    clearInterval(timerInterval);
-       
-}
 },{}]},["4de8m","4B4Nd"], "4B4Nd", "parcelRequirec526")
 
 //# sourceMappingURL=index.a915bf2e.js.map
--- public/scss/components/stopwatch.js
+++ public/scss/components/stopwatch.js
@@ -1,37 +1,35 @@
-// const TIME_LIMIT = 1200;
-// let timePassed = 0;
-// let timeLeft = TIME_LIMIT;
-// let timerInterval = null;
+const TIME_LIMIT = 1200;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
 
+document.getElementById("clock").innerHTML 
 
-
-// function formatTime(time) {
-//     let minutes = Math.floor(time/40)
-//     let seconds = time%60
-//     if (seconds < 10) {
-//         seconds = `0${seconds}`
-//     }
-//     if (minutes < 10) {
-//         minutes = `0${minutes}`
-//     }
-//     return `${minutes}: ${seconds}`
-// }
-// function startTimer() {
-//         timerInterval = setInterval(()=> {
-//             timePassed += 1;
-//             timeLeft= TIME_LIMIT - timePassed;
-//             document.getElementById("label").innerHTML =
-//                 formatTime(timeLeft);
-//             if (timeLeft == 0) {
-//                 stopTimer()
-//             }
+function formatTime(time) {
+    let minutes = Math.floor(time/40)
+    let seconds = time%60
+    if (seconds < 10) {
+        seconds = `0${seconds}`
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    return `${minutes}: ${seconds}`
+}
+function startTimer() {
+        timerInterval = setInterval(()=> {
+            timePassed += 1;
+            timeLeft = TIME_LIMIT - timePassed;
+            document.getElementById("label").innerHTML =
+                formatTime(timeLeft);
+            if (timeLeft == 0) {
+                stopTimer()
+            }
             
-//         },1000);
+        },1000);
  
-// }
-
-
-// function stopTimer() {
-//     clearInterval(timerInterval);
+}
+function stopTimer() {
+    clearInterval(timerInterval);
        
-// }
+}
