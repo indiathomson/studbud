8f2789c8d4170a4d3f4755f438c94ea03e7f5cae
India Thomson <itho5539@uni.sydney.edu.au>
2021-06-11 18:04:02 +0930

    Dictionary
    API is working and connected but only getting data from a hard coded entry word. Input from the dictionary search bar is not being read in the API URL.

--- public/scss/components/dictionary.js
+++ public/scss/components/dictionary.js
@@ -6,15 +6,15 @@ search.addEventListener("click", function(event){
   event.preventDefault();
   let word_ID = wordInput.value;
 
-  console.log(word_ID)
+  console.log("word_ID")
 });
 
 
-var url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=dog"
+var url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry="
 
 var entryWord = 'word_ID';
 
-fetch(url, {
+fetch(url +'word_ID',  {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "cb42d64a92msh6631f571552d438p100823jsn3179a3fd267d",
@@ -38,4 +38,5 @@ fetch(url, {
 	console.error(err);
 });
 
-search.addEventListener("click", fetch);
+search.addEventListener("click", fetch);   
+
